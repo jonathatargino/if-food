0eb7741b01cba441c503d347f2d44c932b8da677
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-04-10 10:39:58 -0300

    feat: create validation middleware

---
+++ if-food-api/src/controllers/middlewares/validator.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { AnySchema, ValidationError } from "yup";
+
+interface RequestValidationSchemas {
+    params?: AnySchema;
+    body?: AnySchema;
+    query?: AnySchema;
+}
+
+export function createValidatorMiddleware(schemas: RequestValidationSchemas) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            for (const [key, schema] of Object.entries(schemas)) {
+                req[key] = (schema as AnySchema).validateSync(req[key]);
+            }
+
+            next();
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: error.errors });
+            }
+
+            console.error(error);
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+        }
+    };
+}
