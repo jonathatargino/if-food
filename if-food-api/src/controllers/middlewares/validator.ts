4f7c2af533bddb0c0652b9e3bba103af1492b1d6
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-04-10 10:47:20 -0300

    fix: not aborting early in middleware validator to return all unprocessable fields

--- if-food-api/src/controllers/middlewares/validator.ts
+++ if-food-api/src/controllers/middlewares/validator.ts
@@ -12,13 +12,17 @@ export function createValidatorMiddleware(schemas: RequestValidationSchemas) {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
             for (const [key, schema] of Object.entries(schemas)) {
-                req[key] = (schema as AnySchema).validateSync(req[key]);
+                req[key] = (schema as AnySchema).validateSync(req[key], {
+                    stripUnknown: true,
+                    abortEarly: false,
+                });
             }
 
             next();
         } catch (error) {
             if (error instanceof ValidationError) {
                 res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({ errors: error.errors });
+                return;
             }
 
             console.error(error);
