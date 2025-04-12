82a422c50d94cc5c42da414ece690bcb640f8cf5
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-04-12 11:09:16 -0300

    feat: add cors

--- if-food-api/src/index.ts
+++ if-food-api/src/index.ts
@@ -1,6 +1,7 @@
 import { getV1Routes } from "@routes/v1";
 import express from "express";
 import http from "node:http";
+import cors from "cors";
 
 export function buildApp() {
     const app = express();
@@ -8,6 +9,7 @@ export function buildApp() {
     app.use(express.json({ limit: "10mb" }));
     app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
+    app.use(cors());
     app.use("/api/v1", getV1Routes());
 
     return http.createServer(app);
