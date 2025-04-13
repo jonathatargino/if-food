0b4bb5d498884e0e64e90e23f8cb1d29f0e7268d
Jonatha Targino <j.oliveiratargino@hotmail.com>
2025-04-12 22:39:23 -0300

    feat: use notistack snackbars in register page

--- if-food-front/src/pages/register/components/form/index.tsx
+++ if-food-front/src/pages/register/components/form/index.tsx
@@ -3,23 +3,22 @@ import { RegisterFormData, schema } from "./schema";
 import { FormProvider, useForm } from "react-hook-form";
 import { UserRole } from "../../../../models/user";
 import { userApi } from "../../../../services/if-food-api/user";
-import { useState } from "react";
-import { Alert, Box, Button, Grid, Link, Snackbar } from "@mui/material";
+import { Box, Button, Grid, Link } from "@mui/material";
 import { Link as RouterLink, useNavigate } from "react-router-dom";
 import { ControlledTextField } from "../../../../components/form/controlled-textfield";
 import { ControlledSelect } from "../../../../components/form/controlled-select";
 import { studyCourseOptions } from "./constants";
 import { ControlledSwitch } from "../../../../components/form/controlled-switch";
+import { useSnackbar } from "notistack";
 
 export function RegisterForm() {
-    const [showSuccess, setShowSuccess] = useState(false);
-    const [showError, setShowError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
     const navigate = useNavigate();
 
     const form = useForm<RegisterFormData>({
         resolver: yupResolver(schema),
     });
+
+    const { enqueueSnackbar } = useSnackbar();
 
     const onSubmit = async (data: RegisterFormData) => {
         try {
@@ -31,16 +30,17 @@ export function RegisterForm() {
                 password: data.password,
                 role: data.is_seller ? UserRole.Seller : UserRole.Customer,
             });
-            console.log(user);
-            setShowSuccess(true);
-            setTimeout(() => {
-                navigate("/login");
-            }, 2000);
+            enqueueSnackbar({
+                message: `Obrigado por se cadastrar, ${user.name}! Faça login para prosseguir`,
+                variant: "success",
+            });
+            navigate("/login");
         } catch {
-            setErrorMessage(
-                "Ocorreu um erro ao criar sua conta. Por favor, tente novamente. Se o problema persistir, contate o suporte",
-            );
-            setShowError(true);
+            enqueueSnackbar({
+                message:
+                    "Ocorreu um erro ao criar sua conta. Por favor, tente novamente. Se o problema persistir, contate o suporte",
+                variant: "error",
+            });
         }
     };
     return (
@@ -108,24 +108,6 @@ export function RegisterForm() {
                         </Link>
                     </Grid>
                 </Grid>
-
-                <Snackbar
-                    open={showSuccess}
-                    autoHideDuration={6000}
-                    onClose={() => setShowSuccess(false)}>
-                    <Alert severity="success" sx={{ width: "100%" }}>
-                        Conta criada com sucesso! Redirecionando para o login...
-                    </Alert>
-                </Snackbar>
-
-                <Snackbar
-                    open={showError}
-                    autoHideDuration={6000}
-                    onClose={() => setShowError(false)}>
-                    <Alert severity="error" sx={{ width: "100%" }}>
-                        {errorMessage}
-                    </Alert>
-                </Snackbar>
             </Box>
         </FormProvider>
     );
